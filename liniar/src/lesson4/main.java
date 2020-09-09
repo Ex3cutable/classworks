f89d6183fe13f61ef90cddb40dcc26235f1437bd
Ex3cutable <70889678+Ex3cutable@users.noreply.github.com>
2020-09-09 10:40:49 +0300

    minor upd

---
+++ liniar/src/lesson4/main.java
@@ -0,0 +1,16 @@
+package lesson4;
+
+public class main {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+
+    }
+
+}
---
+++ liniar/src/lesson5/main.java
@@ -0,0 +1,16 @@
+package lesson5;
+
+
+
+public class main {
+    public static void main(String[] args) {
+        char F = 0x04;
+        byte y = 4;
+        short z = 4;
+        int price = 4;
+        long x = 4;
+        float p = 1.5f;
+        double weight = 1.5;
+
+    }
+}
